4b19f3da0af3f43a4d7fc072b5a50cf2ad338c65
DoubleLift3 <shaquone_10@hotmail.com>
2020-10-09 21:38:06 -0400

    Updated Session and auto loader, uncommented load data code

--- app/IndexController.php
+++ app/IndexController.php
@@ -1,8 +1,15 @@
 <?php 
 
+require "Controller.php";
 class IndexController extends Controller {
 
     public function run(){
-        $this->setModel();
+        $this->setModel(new IndexModel());
+        $this-setView(new View());
+
+        $this->model->view->setTemplate('C:\wamp64\www\FrameworksAssignment\tpl\index.tpl.php');
+        $this->model->attach($this->view);
+
+        
     }
 }
