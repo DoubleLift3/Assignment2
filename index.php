b3c9c37a1d6768e6a40930b3a3ee9f9428a3310f
DoubleLift3 <shaquone_10@hotmail.com>
2020-11-11 17:04:10 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+use Quwi\framework\FrontController;
+
+require 'C:\wamp64\FW_Assignment2\config.php';
+//require_once 'C:wamp64\FW_Assignment2\Database.php';
+require ROOT_DIR . '\autoload.php';
+
+
+
+FrontController::run();
